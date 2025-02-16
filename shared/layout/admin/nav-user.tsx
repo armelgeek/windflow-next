f4ac61bbea57a0b03c719cda2a9d95710dcaa298
Hajavololona Armellin <armelgeek5@gmail.com>
2025-02-16 22:29:08 +0300

    fix: set no name

--- shared/layout/admin/nav-user.tsx
+++ shared/layout/admin/nav-user.tsx
@@ -81,7 +81,7 @@ export function NavUser({
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
+                  <span className="truncate font-semibold">{user.name || 'No name'}</span>
                   <span className="truncate text-xs">{user.email}</span>
                 </div>
               </div>
